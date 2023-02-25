7f092f7168174c636772419c21eef79e2b086f49
Wildanthedev <habsyismart@gmail.com>
2023-02-26 00:52:06 +0700

    video javascript

---
+++ video 21 js/js/shoppingcart.js
@@ -0,0 +1,44 @@
+let tblmenu = [
+{
+    idmenu:1,
+    idkategori:1,
+    menu:"Apel Manalagi",
+    gambar:"apel.jpg",
+    harga:3000,
+},
+{
+    idmenu:2,
+    idkategori:1,
+    menu:"Pisang Raja",
+    gambar:"pisang.jpg",
+    harga:5000, 
+},
+{
+    idmenu:3,
+    idkategori:2,
+    menu:"Nasi Padang",
+    gambar:"nasipadang.jpg",
+    harga:20000,
+},
+{
+    idmenu:4,
+    idkategori:2,
+    menu:"Nasi Ayam",
+    gambar:"nasiayam.jpg",
+    harga:15000,
+},
+{
+    idmenu:5,
+    idkategori:3,
+    menu:"Es Teh",
+    gambar:"esteh.jpg",
+    harga:5000,
+},
+{
+    idmenu:6,
+    idkategori:3,
+    menu:"Es Jeruk",
+    gambar:"esjeruk.jpg",
+    harga:7000,
+},
+];
---
+++ video 3 js/js/operator.js
@@ -0,0 +1,15 @@
+let x = 2;
+
+x = -x;
+
+console.log(x);
+
+let p=5, l=6, t=7, volume;
+
+// p = 5;
+// l = 6;
+// t = 7;
+
+volume = p * l * t;
+
+console.log(volume);
